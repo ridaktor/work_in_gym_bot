b7542a0ec5b4ae72ae52158e9d89aed97d13b475
Danil Konotop <konotopda@gmail.com>
2021-09-17 19:54:34 +0500

    Cleaning code

--- handlers/users/getting_anthropometry.py
+++ handlers/users/getting_anthropometry.py
@@ -1,7 +1,7 @@
 from aiogram import types
 from aiogram.types import CallbackQuery
 from aiogram.dispatcher import FSMContext
-from input_check import answer_validate, state_translate
+from input_check import _answer_validate, _state_translate
 from keyboards.inline.interrupt_buttons import choice
 from keyboards.default.data_buttons import data_keyboard
 from states.anthropometry_states import AnthropometryStates
@@ -13,57 +13,54 @@ from loader import bot, dp
 async def start_getting_anthropometry(message: types.Message, state: FSMContext):
     await message.answer('Сколько ты весишь в кг?', reply_markup=choice)
     await AnthropometryStates.body_weight.set()
-    bot_question_message_id = message.message_id + 1
-    await state.update_data({
-        "bot_question_message_id": bot_question_message_id,
-        "amount_of_answer_attempts": 0
-    })
+    async with state.proxy() as data:
+        data['bot_question_message_id'] = message.message_id + 1
+        data['amount_of_answer_attempts'] = 0
+
 
 for each_state in AnthropometryStates.all_states:
     @dp.message_handler(text="next", state="*")
     @dp.message_handler(state=each_state)
     async def _get_anthropometry(message: types.Message, state: FSMContext):
-        filtered_answer = answer_validate(message.text)
+        filtered_answer = _answer_validate(message.text)
 
-        state_data = await state.get_data()
-        amount_of_answer_attempts = state_data['amount_of_answer_attempts'] + 1  # Counting user answer attempts
-        bot_question_message_id = state_data['bot_question_message_id'] + 2
+        async with state.proxy() as data:
+            amount_of_answer_attempts = data['amount_of_answer_attempts'] + 1
+            bot_question_message_id = data['bot_question_message_id'] + 2
 
         if type(filtered_answer) is not str:
             # Removing the inline keyboard because a valid answer was received
-            bot_question_message_id = state_data['bot_question_message_id'] \
+            bot_question_message_id = data['bot_question_message_id'] \
                                       - amount_of_answer_attempts * 2 + 2  # Revert to the message with the inline keyboard
             await bot.edit_message_reply_markup(chat_id=message.from_user.id,
                                                 message_id=bot_question_message_id, reply_markup=None)
 
             current_state = await state.get_state()
-            translated_state = state_translate(current_state)
-            await state.update_data({
-                translated_state: filtered_answer
-            })
+            translated_state = _state_translate(current_state)
+            async with state.proxy() as data:
+                data[translated_state] = filtered_answer
 
             await AnthropometryStates.next()
             current_state = await state.get_state()
 
             if current_state != 'AnthropometryStates:the_end':
-                translated_state = state_translate(current_state)
+                translated_state = _state_translate(current_state)
                 answer_message = f'Какой у тебя {translated_state} в см?' if translated_state == 'рост' \
                     else f'Какой длины у тебя {translated_state} в см?'
                 await message.answer(answer_message, reply_markup=choice)
-                bot_question_message_id = message.message_id + 1
-                await state.update_data({
-                    "bot_question_message_id": bot_question_message_id,
-                    "amount_of_answer_attempts": 0
-                })
+
+                async with state.proxy() as data:
+                    data['bot_question_message_id'] = message.message_id + 1
+                    data['amount_of_answer_attempts'] = 0
+
             else:
                 await message.answer('Сбор данных полностью завершен', reply_markup=data_keyboard)
                 await state.reset_state(with_data=False)  # finish (reset only state)
         else:
             await message.answer(filtered_answer)
-            await state.update_data({
-                "bot_question_message_id": bot_question_message_id,
-                "amount_of_answer_attempts": amount_of_answer_attempts
-            })
+            async with state.proxy() as data:
+                data['bot_question_message_id'] = bot_question_message_id
+                data['amount_of_answer_attempts'] = amount_of_answer_attempts
 
 
 @dp.callback_query_handler(text="cancel", state="*")
@@ -81,16 +78,14 @@ async def next_getting(call: CallbackQuery, state: FSMContext):
     await call.message.delete_reply_markup()  # Removing the inline keyboard because it has been used
 
     if current_state != 'AnthropometryStates:the_end':
-        translated_state = state_translate(current_state)
+        translated_state = _state_translate(current_state)
         answer_message = f'Какой у тебя {translated_state} в см?' if translated_state == 'рост' \
             else f'Какой длины у тебя {translated_state} в см?'
         await call.message.answer(answer_message, reply_markup=choice)
-        state_id = await state.get_data()
-        bot_question_message_id = state_id['bot_question_message_id'] + 1
-        await state.update_data({
-            "bot_question_message_id": bot_question_message_id,
-            "amount_of_answer_attempts": 0
-        })
+
+        async with state.proxy() as data:
+            data['bot_question_message_id'] += 1
+            data['amount_of_answer_attempts'] = 0
     else:
         await call.message.answer('Сбор данных завершен', reply_markup=data_keyboard)
         await state.reset_state(with_data=False)
--- input_check.py
+++ input_check.py
@@ -1,25 +1,32 @@
 import re
 
 
-def answer_validate(answer):
-    """Validates the user response for the presence of numbers"""
+def _number_search(answer: str):
+    """Searches for a number in a string"""
     filtered_answer = answer.replace(',', '.')
-    # Replacing Cyrillic 'е' with 'Е' in the case of an exponential form
-    filtered_answer = filtered_answer.replace('\u0435', 'e').replace('\u0415', 'E')
-    try:
-        # Searching for a real number (including exponential form)
-        filtered_answer = re.search(r"[-+]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][-+]?\d+)?", filtered_answer)
-
-        filtered_answer = round(float(filtered_answer.group(0)), 1)  # Round up for a clean display
-        if filtered_answer > 0:
-            return filtered_answer if filtered_answer % 1 > 0 else int(filtered_answer)  # for a clean display
-        else:
-            return "Значние отрицательное или слишком мало, попробуй еще раз"
-    except:
+    filtered_answer = filtered_answer.replace('\u0435', 'e').replace('\u0415', 'E')  # Replacing Cyrillic 'е' and 'Е'
+    filtered_answer = re.search(r"[-+]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][-+]?\d+)?", filtered_answer)
+    if filtered_answer:
+        return round(float(filtered_answer.group(0)), 1)
+    else:
         return "Неверный формат, попробуй еще раз"
 
 
-def state_translate(name_of_state):
+def _answer_validate(answer: str):
+    """Validates the user answer in terms of a positive number"""
+    if answer.isdigit() and int(answer) > 0:
+        return int(answer)
+    else:
+        filtered_answer = _number_search(answer)
+        if isinstance(filtered_answer, str):
+            return filtered_answer
+        elif filtered_answer > 0:
+            return filtered_answer if filtered_answer % 1 > 0 else int(filtered_answer)  # for a clean display
+        else:
+            return "Значние отрицательное или слишком мало, попробуй еще раз"
+
+
+def _state_translate(name_of_state: str) -> str:
     """Translates the name of state into Russian"""
     result = re.findall(r'\w+$', name_of_state)
     sub_dict = {
